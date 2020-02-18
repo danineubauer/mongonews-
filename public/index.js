6bb8d213fdfdb0887bb5aff3381dbd64a87764fb
Danielle <danineubauerr@gmail.com>
2020-02-17 20:23:24 -0800

    adds text and links

--- public/index.js
+++ public/index.js
@@ -1,38 +1,76 @@
-$(document).ready(function() { 
 
-    //article-box: 
+function displayResults(articles) { 
+    console.log('displaying data')
+    articles.forEach (function(article) { 
+        // console.log(article.title)
+        // console.log(article.summary)
+        // console.log(article.link)
 
-    var articleBox = $('#article-box'); 
+        var div = $("<div>").append(
+            $("<a href='" + article.link + "'>").text(article.title), 
+            $("<div>").text(article.summary),
+        )
+        $('#addArticleInfoHere').append(div)
+    })
+}
+
+$(".scrape").on("click", function() { 
     
-    //button actions: 
-    $(document).on("click", ".scrape", scrapeArticlesHandle); 
-    $(document).on("click", "#clear", clearArticlesHandle); 
-    $(document).on("click", "#save", saveArticlesHandler); 
-
-    function scrapeArticlesHandle(articles) {
-        console.log('scrape clicked') 
-        articles.forEach(function(article) {
-            var div = $('<div>').append(
-                $("<div>").text(articles.title),
-                $("<div>").text(articles.summary), 
-                $("<div>").text(articles.link)
-            );
-            $("#addArticleInfoHere").append(div)
-        });
-        
-        //appending: 
-    };
-
-
-    function clearArticlesHandle(articles) { 
-        console.log('clear article button clicked')
-    }
-
-    function saveArticlesHandler(articles) { 
-        console.log('save article button clicked')
-    }
+    console.log('scraping data')
     
+    $.getJSON('/all', function(data) {
+        displayResults(data) 
+    });
 });
+
+
+    // $.getJSON("/scrape", function(data) { 
+    //     displayResults(data)
+    //     console.log('displays data')
+    // });
+
+
+
+
+
+
+
+// $(document).ready(function() { 
+
+//     //article-box: 
+
+//     var articleBox = $('#article-box'); 
+    
+//     //button actions: 
+//     $(document).on("click", ".scrape", scrapeArticlesHandle); 
+//     $(document).on("click", "#clear", clearArticlesHandle); 
+//     $(document).on("click", "#save", saveArticlesHandler); 
+
+//     function scrapeArticlesHandle(articles) {
+//         console.log('scrape clicked') 
+  
+//         db.articles.forEach(function(article) {
+//             var div = $('<div>').append(
+//                 $("<div>").text(article.title),
+//                 $("<div>").text(article.summary), 
+//                 $("<div>").text(article.link)
+//             );
+//             $("#addArticleInfoHere").append(div)
+//         });
+        
+//         //appending: 
+//     };
+
+
+//     function clearArticlesHandle(articles) { 
+//         console.log('clear article button clicked')
+//     }
+
+//     function saveArticlesHandler(articles) { 
+//         console.log('save article button clicked')
+//     }
+
+// });
 
 
 
